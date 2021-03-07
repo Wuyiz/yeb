1a00a9ac74cbc9565faa292fa8b478e6e3c468fd
Wuyiz <wuyiz@foxmail.com>
2021-03-07 23:11:10 +0800

    feat(JWT授权模块、统一返回结果集): 添加统一返回结果类，增加jwt工具类

---
+++ yeb-server/src/main/java/com/wuyiz/server/config/security/JwtTokenUtil.java
@@ -0,0 +1,157 @@
+package com.wuyiz.server.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA
+ * @author: suhai
+ * @date: 2021-03-07 17:23
+ * @description: jwt工具类
+ *
+ * 可供外界调用的方法：
+ *      1. generateToken
+ *      2. getUserNameFromToken
+ *      3. validateToken
+ *      4. canRefresh
+ *      5. refreshToken
+ */
+@Component
+public class JwtTokenUtil {
+    //荷载常量
+    private static final String CLAIM_KEY_USERNAME = "sub";
+    private static final String CLAIM_KEY_CREATED = "created";
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    /**
+     * 根据用户信息生成token
+     * @param userDetails
+     * @return
+     */
+    public String generateToken(UserDetails userDetails) {
+        //创建载荷信息集合map
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(CLAIM_KEY_USERNAME, userDetails.getUsername());
+        claims.put(CLAIM_KEY_CREATED, new Date());
+        return generateToken(claims);
+    }
+
+    /**
+     * 从token中获取登录用户名
+     * @param token
+     * @return
+     */
+    public String getUserNameFromToken(String token) {
+        String username;
+        try {
+            Claims claims = getClaimFormToken(token);
+            username = claims.getSubject();
+        } catch (Exception e) {
+            username = null;
+        }
+        return username;
+    }
+
+    /**
+     *
+     * @param token
+     * @param userDetails
+     * @return
+     */
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String username = getUserNameFromToken(token);
+        return username.equals(userDetails.getUsername()) && isTokenExpired(token);
+    }
+
+    /**
+     * 判断token是否可以被刷新
+     * @param token
+     * @return
+     */
+    public boolean canRefresh(String token) {
+        return !isTokenExpired(token);
+    }
+
+    /**
+     * 更新token创建日期并重新生成token
+     * @param token
+     * @return
+     */
+    public String refreshToken(String token) {
+        Claims claims = getClaimFormToken(token);
+        claims.put(CLAIM_KEY_CREATED, new Date());
+        return generateToken(claims);
+    }
+
+    /**
+     * 判断token是否失效
+     * @param token
+     * @return
+     */
+    private boolean isTokenExpired(String token) {
+        Date expiredDate = getExpiredDateFromToken(token);
+        return expiredDate.before(new Date());
+    }
+
+    /**
+     * 获取token中的过期时间
+     * @param token
+     * @return
+     */
+    private Date getExpiredDateFromToken(String token) {
+        return getClaimFormToken(token).getExpiration();
+    }
+
+    /**
+     * 从token中获取载荷信息
+     * @param token
+     * @return
+     */
+    private Claims getClaimFormToken(String token) {
+        Claims claims = null;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return claims;
+    }
+
+
+    /**
+     * 根据用户信息生成jwt token
+     * @param claims
+     * @return
+     */
+    private String generateToken(Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(generateExpirationDate())
+                .signWith(SignatureAlgorithm.ES256, secret)
+                .compact();
+    }
+
+    /**
+     * 生成token失效时间
+     * @return
+     */
+    private Date generateExpirationDate() {
+        return new Date(System.currentTimeMillis() + expiration);
+    }
+
+
+}
---
+++ yeb-server/src/main/java/com/wuyiz/server/utils/RespPageBena.java
@@ -0,0 +1,21 @@
+package com.wuyiz.server.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA
+ * @author: suhai
+ * @date: 2021-03-07 19:48
+ * @description: 分页公共返回对象
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RespPageBena {
+    private Long total;
+    private List<?> data;
+}
