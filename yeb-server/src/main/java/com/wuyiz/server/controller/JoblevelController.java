cea2174dabb526a6d6a0cb89315563e94109e612
Wuyiz <wuyiz@foxmail.com>
2021-03-10 13:36:58 +0800

    feat(职称、职位、全局异常处理): 增加了基础信息设置菜单中的职称和职位的功能模块，添加了sql全局异常处理模块

--- yeb-server/src/main/java/com/wuyiz/server/controller/JoblevelController.java
+++ yeb-server/src/main/java/com/wuyiz/server/controller/JoblevelController.java
@@ -1,8 +1,16 @@
 package com.wuyiz.server.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wuyiz.server.pojo.Joblevel;
+import com.wuyiz.server.service.JoblevelService;
+import com.wuyiz.server.utils.RespBean;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>
@@ -13,7 +21,51 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-03-06
  */
 @RestController
-@RequestMapping("/joblevel")
+@RequestMapping("system/basic/joblevel")
 public class JoblevelController {
+    @Autowired
+    JoblevelService joblevelService;
 
+    @ApiOperation(value = "查询所有职称等级列表")
+    @GetMapping
+    public List<Joblevel> getAllPosition() {
+        return joblevelService.list();
+    }
+
+    @ApiOperation(value = "添加职称等级信息")
+    @PostMapping
+    public RespBean addPosition(@RequestBody Joblevel joblevel) {
+        joblevel.setCreateDate(LocalDateTime.now());
+        if (joblevelService.save(joblevel)) {
+            return RespBean.success("添加成功");
+        }
+        return RespBean.error("添加失败");
+    }
+
+    @ApiOperation(value = "更新职称等级信息")
+    @PutMapping
+    public RespBean updatePosition(@RequestBody Joblevel joblevel) {
+        if (joblevelService.updateById(joblevel)) {
+            return RespBean.success("更新成功");
+        }
+        return RespBean.error("更新失败");
+    }
+
+    @ApiOperation(value = "删除职称等级信息")
+    @DeleteMapping("/{id}")
+    public RespBean deletePosition(@PathVariable Integer id) {
+        if (joblevelService.removeById(id)) {
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
+
+    @ApiOperation(value = "批量删除职称等级信息")
+    @DeleteMapping
+    public RespBean deletePositionList(Integer[] ids) {
+        if (joblevelService.removeByIds(Arrays.asList(ids))) {
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
 }
